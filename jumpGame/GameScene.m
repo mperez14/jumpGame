aa6ddbfe2530e3121ef25dc63cbf4d55a6101727
mperez14 <mperez14@nd.edu>
2015-04-24 01:08:42 -0400

    added moving cow

--- jumpGame/GameScene.m
+++ jumpGame/GameScene.m
@@ -26,12 +26,13 @@
         
         [self initScrollingGround];
         [self initScrollingClouds];
+        [self addCow];
     }
     return self;
 }
 
 -(void)initScrollingGround{ //Scrolling tracks function
-    SKTexture *groundTexture = [SKTexture textureWithImageNamed:@"grass2.png"]; //change runway to train tracks
+    SKTexture *groundTexture = [SKTexture textureWithImageNamed:@"grass_1.png"]; //change runway to train tracks
     SKAction *moveGroundSprite = [SKAction moveByX:-groundTexture.size.width*2 y:0 duration:.02*groundTexture.size.width*2];
     SKAction *resetGroundSprite = [SKAction moveByX:groundTexture.size.width*2 y:0 duration:0];
     SKAction *moveGroundSpriteForever = [SKAction repeatActionForever:[SKAction sequence:@[moveGroundSprite, resetGroundSprite]]];
@@ -41,7 +42,7 @@
         [sprite setScale:1];
         sprite.zPosition = 10;
         sprite.anchorPoint = CGPointZero;
-        sprite.position = CGPointMake(i*sprite.size.width, -50);
+        sprite.position = CGPointMake(i*sprite.size.width, 0);
         [sprite runAction:moveGroundSpriteForever];
         [_characterLayer addChild:sprite];
     }
@@ -50,9 +51,9 @@
 -(void)initScrollingClouds{   //scrolling CLOUDS function
     SKTexture *backgroundTexture = [SKTexture textureWithImageNamed:@"Clouds.png"];        //reuse sky image
     SKAction *moveBg= [SKAction moveByX:-backgroundTexture.size.width y:0 duration: 0.1*backgroundTexture.size.width]; //move Bg
-    SKAction *resetBg = [SKAction moveByX:backgroundTexture.size.width*2 y:0 duration:0];   //reset background
+    SKAction *resetBg = [SKAction moveByX:backgroundTexture.size.width y:0 duration:0];   //reset background
     SKAction *moveBackgroundForever = [SKAction repeatActionForever:[SKAction sequence:@[moveBg, resetBg]]];    //repeat moveBg and resetBg
-    for(int i =0; i<2+self.frame.size.width/(backgroundTexture.size.width*2); i++){     //place image
+    for(int i =0; i<2+self.frame.size.width/(backgroundTexture.size.width); i++){     //place image
         SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithTexture:backgroundTexture];
         [sprite setScale:1];
         sprite.zPosition=-20;
@@ -61,6 +62,18 @@
         [sprite runAction:moveBackgroundForever];
         [_bgLayer addChild:sprite];
     }
+}
+
+-(void)addCow{
+    cow = [SKSpriteNode spriteNodeWithImageNamed:@"Cow.png"];//change to train png
+    cow.name = @"cow";
+    cow.position = CGPointMake(400, 50);
+    cow.zPosition = -5;
+    cow.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(50, 50)];
+    cow.physicsBody.affectedByGravity = NO;
+    cow.physicsBody.allowsRotation = NO;
+    [_characterLayer addChild:cow];
+    [cow.physicsBody applyForce:CGVectorMake(-25, 0)];
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
