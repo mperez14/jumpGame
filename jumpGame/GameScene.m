3f4f629d16081ece0dc07aab6939a2df8bf67816
mperez14 <mperez14@nd.edu>
2015-04-24 00:32:33 -0400

    scrolling ground function

--- jumpGame/GameScene.m
+++ jumpGame/GameScene.m
@@ -8,11 +8,57 @@
 
 #import "GameScene.h"
 
-@implementation GameScene
-
--(void)didMoveToView:(SKView *)view {
-    /* Setup your scene here */
+@implementation GameScene{
+    SKSpriteNode *cow;
+    SKNode *_bgLayer;
+    SKNode *_characterLayer;
+}
+/****MAIN****/
+-(id)initWithSize:(CGSize)size{
     
+    
+    if(self = [super initWithSize:size]){
+        _bgLayer = [SKNode node];       //init layers
+        [self addChild:_bgLayer];
+        _characterLayer = [SKNode node];
+        [self addChild:_characterLayer];
+        
+        [self initScrollingGround];
+    }
+    return self;
+}
+
+-(void)initScrollingGround{ //Scrolling tracks function
+    SKTexture *groundTexture = [SKTexture textureWithImageNamed:@"grass.png"]; //change runway to train tracks
+    SKAction *moveGroundSprite = [SKAction moveByX:-groundTexture.size.width*2 y:0 duration:.02*groundTexture.size.width*2];
+    SKAction *resetGroundSprite = [SKAction moveByX:groundTexture.size.width*2 y:0 duration:0];
+    SKAction *moveGroundSpriteForever = [SKAction repeatActionForever:[SKAction sequence:@[moveGroundSprite, resetGroundSprite]]];
+    
+    for(int i=0; i<2 +self.frame.size.width/(groundTexture.size.width);i++){      //place image
+        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithTexture:groundTexture];
+        [sprite setScale:1];
+        sprite.zPosition = 10;
+        sprite.anchorPoint = CGPointZero;
+        sprite.position = CGPointMake(i*sprite.size.width, 0);
+        [sprite runAction:moveGroundSpriteForever];
+        [_characterLayer addChild:sprite];
+    }
+}
+
+-(void)initScrollingClouds{   //scrolling CLOUDS function
+    SKTexture *backgroundTexture = [SKTexture textureWithImageNamed:@"Clouds.png"];        //reuse sky image
+    SKAction *moveBg= [SKAction moveByX:-backgroundTexture.size.width y:0 duration: 0.1*backgroundTexture.size.width]; //move Bg
+    SKAction *resetBg = [SKAction moveByX:backgroundTexture.size.width*2 y:0 duration:0];   //reset background
+    SKAction *moveBackgroundForever = [SKAction repeatActionForever:[SKAction sequence:@[moveBg, resetBg]]];    //repeat moveBg and resetBg
+    for(int i =0; i<2+self.frame.size.width/(backgroundTexture.size.width*2); i++){     //place image
+        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithTexture:backgroundTexture];
+        [sprite setScale:1];
+        sprite.zPosition=-20;
+        sprite.anchorPoint=CGPointZero;
+        sprite.position=CGPointMake(i*sprite.size.width, 500);
+        [sprite runAction:moveBackgroundForever];
+        [_bgLayer addChild:sprite];
+    }
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
