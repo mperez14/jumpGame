106ab888fb3f7960f173ed5cd707ee28832ebae3
mperez14 <mperez14@nd.edu>
2015-04-24 00:05:17 -0400

    Initial Commit

---
+++ jumpGame/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  jumpGame
+//
+
+//  Copyright (c) 2015 Matthew Perez. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ jumpGame/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  jumpGame
+//
+
+//  Copyright (c) 2015 Matthew Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
